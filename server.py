72269ae54e21815803a828097c43998291d9bbf3
tosaki <t0saki@outlook.com>
2024-06-05 00:37:24 +0800

    add index to server

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, abort, request, jsonify, send_from_directory
 from flask_cors import CORS
 from Index.index_utils import *
 import warnings
+import os
 
 warnings.filterwarnings("ignore")
 
 image_collection, text_collection = create_vectordb("db")
-original_paths, os_paths = get_images_paths("images_paths.txt")
+original_paths, os_paths = get_images_paths("db/image_index.db")
 index_images(os_paths, original_paths, image_collection, text_collection)
 clean_index(original_paths, image_collection, text_collection)
 
@@ -28,6 +29,7 @@ def search_clip_text(text, image_collection):
     paths = results["ids"][0]
     return paths, distances
 
+
 def search_clip_image(image_path, image_collection, get_self=False):
     """
     Search for images that are visually similar to the input image within a given image collection.
@@ -40,7 +42,7 @@ def search_clip_image(image_path, image_collection, get_self=False):
         tuple: A tuple containing two lists. The first list contains the paths of the top 5 images (or top 6 if get_self is True). The second list contains the corresponding distances of these images from the input image.
     """
     image_path = image_path.strip('"').strip("'")
-    if os.name == "posix": 
+    if os.name == "posix":
         image_path = image_path.replace("\\", "/").replace("C:", "/mnt/c")
     image_embedding = get_clip_image(image_path)
     if not get_self:
@@ -52,6 +54,7 @@ def search_clip_image(image_path, image_collection, get_self=False):
         distances = results["distances"][0]
         paths = results["ids"][0]
     return paths, distances
+
 
 def search_embed_text(text, text_collection):
     """
@@ -72,7 +75,7 @@ def search_embed_text(text, text_collection):
 
 
 # Flask App
-app = Flask(__name__)
+app = Flask(__name__, static_folder="UI/CLIPPyX WebUI")
 CORS(app)
 
 
@@ -83,6 +86,7 @@ def clip_text_route():
     # for path, distance in zip(paths, distances):
     #     print(f"Path: {path}, Distance: {distance}")
     return jsonify(paths)
+
 
 @app.route("/clip_image", methods=["POST"])
 def clip_image_route():
@@ -102,5 +106,20 @@ def ebmed_text_route():
     return jsonify(paths)
 
 
+@app.route("/index")
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/images/<path:filename>")
+def serve_image(filename):
+    filename = os.path.join("/", filename)
+    # # Not needed for now
+    # LOCAL_IMAGE_DIR = os.getenv("LOCAL_IMAGE_DIR")
+    # assert filename.startswith(LOCAL_IMAGE_DIR)
+    return send_from_directory(os.path.dirname(filename), os.path.basename(filename))
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
