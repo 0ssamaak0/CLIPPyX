2a743f2f124da8dff4a106b49a18ea73383a8fd2
0ssamaak0 <0ssamaak0@gmail.com>
2024-06-09 00:25:12 +0300

    removing unecessary files

--- test.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-from tqdm import tqdm
-
-chroma_client = chromadb.PersistentClient(path="windows_db")
-collection = chroma_client.get_or_create_collection(name="my_collection")
-
-for i in tqdm(range(200)):
-    collection.upsert(ids = str(i), embeddings = [i, i, i])
