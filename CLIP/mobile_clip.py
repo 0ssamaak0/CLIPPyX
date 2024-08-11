675aff7ba5adc561c5589f30f35dec96a941acd1
0ssamaak0 <0ssamaak0@gmail.com>
2024-08-12 01:56:58 +0300

    batch processing of images in mobileclip (both model and io)

--- CLIP/mobile_clip.py
+++ CLIP/mobile_clip.py
@@ -1,15 +1,20 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 import torch
 from PIL import Image
 import mobileclip
 import os
 import yaml
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # Load the configuration file
-with open('config.yaml', 'r') as f:
+with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
-checkpoint = config['clip']['mobileclip_checkpoint']
+checkpoint = config["clip"]["mobileclip_checkpoint"]
+
 
 def download_mobile_clip(checkpoint):
     """
@@ -33,6 +38,8 @@ def download_mobile_clip(checkpoint):
         unit_scale=True,
         unit_divisor=1024,
     )
+    # create checkpoints path if it doesn't exist
+    os.makedirs("checkpoints", exist_ok=True)
     with open(f"checkpoints/{filename}", "wb") as f:
         for data in progress.iterable:
             f.write(data)
@@ -50,24 +57,51 @@ model, _, preprocess = mobileclip.create_model_and_transforms(
 tokenizer = mobileclip.get_tokenizer(checkpoint)
 # move model to cuda if available
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
 model.to(device)
 
 
-def get_clip_image(image_path):
+def preprocess_image(image_path):
     """
-    Gets the image embeddings for the given image path.
+    Opens and preprocesses a single image.
 
     Args:
         image_path (str): The path to the image file.
 
     Returns:
-        list: The image embeddings as a list.
+        torch.Tensor: The preprocessed image tensor.
     """
     image = Image.open(image_path)
-    image = preprocess(image.convert("RGB")).unsqueeze(0).to(device)
-    with torch.no_grad(), torch.cuda.amp.autocast():
-        image_features = model.encode_image(image)
-    return image_features.cpu().squeeze(0).numpy().tolist()
+    return preprocess(image.convert("RGB"))
+
+
+def get_clip_image(image_paths):
+    """
+    Computes the image embeddings for a batch of images and calculates the average pixel value of the first channel of each image.
+
+    This function opens each image from the specified paths, preprocesses them for the model, and computes their embeddings. Additionally, it calculates the average pixel value of the first channel as a simple feature for each image.
+
+    Args:
+        image_paths (list): List of image paths with length equal to batch_size
+
+    Returns:
+        list: A list containing the image embeddings for each image in the batch.
+    """
+
+    # Use ThreadPoolExecutor to preprocess images in parallel
+    with ThreadPoolExecutor() as executor:
+        preprocessed_images = list(executor.map(preprocess_image, image_paths))
+    # Stack the preprocessed images into a tensor
+    images_tensor = torch.stack(preprocessed_images).to(device)
+
+    # Encode the batch of images
+    with torch.no_grad(), torch.amp.autocast("cpu"):
+        image_features_batch = model.encode_image(images_tensor)
+
+    # Convert the batch of image features to a list of lists
+    image_features_list = image_features_batch.cpu().squeeze(1).numpy().tolist()
+
+    return image_features_list
 
 
 def get_clip_text(text):
@@ -81,6 +115,6 @@ def get_clip_text(text):
         list: The text embeddings as a list.
     """
     text = tokenizer([text]).to(device)
-    with torch.no_grad(), torch.cuda.amp.autocast():
+    with torch.no_grad(), torch.amp.autocast("cpu"):
         text_features = model.encode_text(text)
     return text_features.cpu().squeeze(0).numpy().tolist()
