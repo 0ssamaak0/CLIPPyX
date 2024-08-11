8ebc0969af95256d510cdfa028060db28258765b
0ssamaak0 <0ssamaak0@gmail.com>
2024-08-12 02:23:09 +0300

    batch processing of images in HF transformers CLIP (both model and io)

--- CLIP/hftransformers_clip.py
+++ CLIP/hftransformers_clip.py
@@ -1,12 +1,13 @@
 from transformers import CLIPProcessor, CLIPModel
+from concurrent.futures import ThreadPoolExecutor
 import torch
 from PIL import Image
 import yaml
 
 # Load the configuration file
-with open('config.yaml', 'r') as f:
+with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
-checkpoint = config['clip']['transformers_clip_checkpoint']
+checkpoint = config["clip"]["transformers_clip_checkpoint"]
 
 # load model and tokenizer
 model = CLIPModel.from_pretrained(checkpoint)
@@ -15,21 +16,40 @@ processor = CLIPProcessor.from_pretrained(checkpoint)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
-def get_clip_image(image_path):
+
+def open_image(image_path):
     """
-    Gets the image embeddings for the given image path.
+    Opens an image from the specified file path.
 
     Args:
         image_path (str): The path to the image file.
 
     Returns:
-        list: The image embeddings as a list.
+        Image: An Image object representing the opened image.
     """
     image = Image.open(image_path)
+    return image
+
+
+def get_clip_image(image_paths):
+    """
+    Computes the image embeddings for a batch of images and calculates the average pixel value of the first channel of each image.
+
+    This function opens each image from the specified paths, preprocesses them for the model, and computes their embeddings. Additionally, it calculates the average pixel value of the first channel as a simple feature for each image.
+
+    Args:
+        image_paths (list): List of image paths with length equal to batch_size
+
+    Returns:
+        list: A list containing the image embeddings for each image in the batch.
+    """
+    with ThreadPoolExecutor() as executor:
+        images = list(executor.map(open_image, image_paths))
+    processed_image = processor(images=images, return_tensors="pt").to(device)
     with torch.no_grad():
-        processed_image = processor(images=image, return_tensors="pt").to(device)
         image_features = model.get_image_features(**processed_image)
     return image_features.cpu().squeeze(0).numpy().tolist()
+
 
 def get_clip_text(text):
     """
