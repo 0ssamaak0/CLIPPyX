ec49c7d32e97bd12ce57bf9558e57982bac15b19
0ssamaak0 <0ssamaak0@gmail.com>
2024-08-13 21:34:37 +0300

    -updating create_db with new embedding providers -updated requirements.txt file

--- Index/create_db.py
+++ Index/create_db.py
@@ -15,17 +15,19 @@ with open("config.yaml", "r") as f:
 deep_scan = config["deep_scan"]
 batch_size = config["batch_size"]
 
-if config["clip"]["provider"] == "transformers":
+if config["clip"]["provider"] == "HF_transformers":
     from CLIP.hftransformers_clip import get_clip_image, get_clip_text
 elif config["clip"]["provider"] == "mobileclip":
     from CLIP.mobile_clip import get_clip_image, get_clip_text
 
-if config["text_embed"]["provider"] == "transformers":
+if config["text_embed"]["provider"] == "HF_transformers":
     from text_embeddings.hftransformers_embeddings import get_text_embeddings
 elif config["text_embed"]["provider"] == "ollama":
     from text_embeddings.ollama_embeddings import get_text_embeddings
-if config["text_embed"]["provider"] == "llama_cpp":
+elif config["text_embed"]["provider"] == "llama_cpp":
     from text_embeddings.llamacpp_embeddings import get_text_embeddings
+elif config["text_embed"]["provider"] == "openai_api":
+    from text_embeddings.openai_api import get_text_embeddings
 from ocr_model.OCR import apply_OCR
 
 
