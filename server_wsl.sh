4103313f96d95fca094ccc344cb45fc4bcd5e083
0ssamaak0 <0ssamaak0@gmail.com>
2024-08-13 21:33:47 +0300

    dropping wsl support

--- server_wsl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# (if you are using conda, else comment the line)
-# source <path_to_conda_bin>/activate <env_name> 
-source /home/ossamaak0/miniconda3/bin/activate main
-# The next line runs the server
-python3 server.py
