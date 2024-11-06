2124eddc541dee15cb694a9afcd162a3eb62ab6f
0ssamaak0 <0ssamaak0@gmail.com>
2024-11-07 00:23:43 +0200

    fixing issue when running from venv

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import subprocess
+import sys
 import argparse
 import shutil
 import os
 import platform
+
+# Path to the Python executable using the same interpreter that's running this script
+python_executable = sys.executable
+
+
+def run_script(script_name):
+    result = subprocess.run([python_executable, script_name])
+
+    if result.returncode != 0:
+        print(f"Error running {script_name}: {result.stderr}")
+        return False
+    else:
+        print(f"Successfully ran {script_name}: {result.stdout}")
+        return True
+
 
 # if it gets the flag --settings, open settings.py
 parser = argparse.ArgumentParser()
@@ -15,7 +31,7 @@ parser.add_argument("--open-config-file", action="store_true", help="open config
 args = parser.parse_args()
 
 if args.settings:
-    subprocess.run(["python", "settings.py"])
+    run_script("settings.py")
     exit()
 
 if args.delete_index:
@@ -42,5 +58,6 @@ if args.open_config_file:
         os.system("xdg-open config.yaml")
     exit()
 
-subprocess.run(["python", "create_index.py"])
-subprocess.run(["python", "server.py"])
+# Run create_index.py and check if it succeeds before running server.py
+run_script(script_name="create_index.py")
+run_script("server.py")
